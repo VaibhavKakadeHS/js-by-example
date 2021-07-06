74ef46190d4325078fd9f6e9ac00da121b9a9038
VaibhavKakade <vaibhavkakade@helpshift.com>
2021-07-06 15:14:12 +0530

    Modify event description

--- webpage-basics/index.js
+++ webpage-basics/index.js
@@ -1,11 +1,12 @@
-// This event is fired soon as the page DOM has been loaded, without
-// waiting for resources to finish loading
-window.addEventListener("load", function (event) {
-  console.log("page is fully loaded");
+// This event is fired when the whole page has loaded, including
+// all dependent resources such as stylesheets and images
+window.addEventListener("load", function () {
+console.log("Page is fully loaded");
 });
 
-// This event is fired as soon as the page DOM has been loaded, without
-// waiting for resources to finish loading
+// This event is fired when the initial HTML document has been completely
+// loaded and parsed, without waiting for stylesheets, images,
+// and subframes to finish loading.
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Document is loaded");
 });
